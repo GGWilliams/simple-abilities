ac5de8421f0660e9f0c93292e39fb601aaec29b0
George Williams <huxous@gmail.com>
2022-05-26 17:22:56 -0700

    add useState to page0 for setting an image from a url

--- client/src/components/Page0.jsx
+++ client/src/components/Page0.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import Axios from "axios";
 function Page0(){
-	return <div className="container">Page 0 yo</div>
+
+	const [image, setImage] = useState("https://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/Azir.png");
+	const getImage = () => {
+
+		setImage("https://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/Annie.png")
+
+	}
+	return <div className="container">
+		<img src={image} alt=""  />
+		<button onClick={getImage}>Change Image</button>
+	</div>
 }
 export default Page0;
