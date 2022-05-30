643fa1df5608565ab37839ff3b2b77d999e91b90
George Williams <huxous@gmail.com>
2022-05-29 19:47:06 -0700

    add .env to hide API key, add axios get request to riot games api server side, add axios get request inside page0.js to access simple-abilities server

--- client/src/components/Page0.jsx
+++ client/src/components/Page0.jsx
@@ -1,16 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
-import Axios from "axios";
+import "./Page0.css";
+
+const axios = require("axios");
+
 function Page0(){
 
 	const [image, setImage] = useState("https://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/Azir.png");
+	const [summonerIconCode, setSummonerIconCode] = useState("testing");
+
 	const getImage = () => {
 
 		setImage("https://ddragon.leagueoflegends.com/cdn/10.25.1/img/champion/Annie.png")
 
 	}
+
+	axios.get("http://localhost:3001/participants")
+	.then(function (response) {
+		setSummonerIconCode(response.data.participants[0].summonerName);
+		console.log(response.data.participants[0].summonerName);
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error);
+	})
+	.then(function () {
+		// always executed
+	});
+
+
 	return <div className="container">
 		<img src={image} alt=""  />
+		<h1>{summonerIconCode}</h1>
+
 		<button onClick={getImage}>Change Image</button>
 	</div>
 }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3001;
+const axios = require("axios");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +14,36 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   })
 }
+
+
+
+
+
+
+
+app.get("/participants", (req, res) => {
+
+  axios.get("https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/Anj-JuLC1iXuvjX54jP8KKw6hW0EIyTZZNZiB4tL2NgMVwI?api_key=" + process.env.TOP_SECRET_API_KEY)
+  .then(function (response) {
+    res.json(response.data);
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+})
+
+
+
+
+
+
+
+
 
 app.listen(port, function() {
   console.log("express server is running on port 3001 or env")
